0ca8426cd26abb6b09cd092b12ea541be38b3274
matejgeljic <32310104+matejgeljic@users.noreply.github.com>
2018-07-22 18:54:10 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
     });
 
     //scroll on buttons
-
     $('.js--scroll-to-plans').click(function(){
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
     });
@@ -51,4 +50,48 @@ $(document).ready(function(){
     }
   });
 
+    //animations on scroll
+  $('.js--wp-1').waypoint(function(direction){
+      $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+      offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction){
+    $('.js--wp-2').addClass('animated fadeInUp');
+}, {
+    offset: '50%'
 });
+
+  $('.js--wp-3').waypoint(function(direction){
+    $('.js--wp-3').addClass('animated fadeIn');
+}, {
+    offset: '50%'
+});
+
+  $('.js--wp-4').waypoint(function(direction){
+    $('.js--wp-4').addClass('animated pulse');
+}, {
+    offset: '50%'
+});
+
+    //mobile nav
+    $('.js--nav-icon').click(function(){
+        let nav = $('.js--header__nav');
+        let icon = $('.js--nav-icon i');
+
+        nav.slideToggle(200);
+        if(icon.hasClass('ion-md-menu')){
+            icon.addClass('ion-md-close');
+            icon.removeClass('ion-md-menu');
+        } else{
+            icon.addClass('ion-md-menu');
+            icon.removeClass('ion-md-close');
+        }
+    });
+
+});
+
+
+
+
